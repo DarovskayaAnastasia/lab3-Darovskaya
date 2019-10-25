d5c79fc04e2c4cb54b5190d41c77e31ab6ef9a57
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 18:34:33 +0000

    Scripted auto-commit on change (2019-10-25 18:34:33) by gitwatch.sh

--- src/main/java/AirportsSparkMain.java
+++ src/main/java/AirportsSparkMain.java
@@ -1,18 +1,18 @@
-//import org.apache.spark.SparkConf;
-//import org.apache.spark.api.java.JavaPairRDD;
-//import org.apache.spark.api.java.JavaRDD;
-//import org.apache.spark.api.java.JavaSparkContext;
-//import scala.Tuple2;
-//
-//public class AirportsSparkMain {
-//    public static void main(String[] args) {
-//        SparkConf conf = new SparkConf().setAppName("lab3");
-//        JavaSparkContext sc = new JavaSparkContext(conf);
-//
-//        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
-//        JavaRDD<String> onTimeFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-//
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class AirportsSparkMain {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> onTimeFile = sc.textFile("664600583_T_ONTIME_sample.csv");
+
 //        JavaPairRDD<String, Long> nameIDpair = airportsFile.mapToPair(s -> {} new Tuple2<>(airportID, airportName));
-//
-//    }
-//}
+
+    }
+}
