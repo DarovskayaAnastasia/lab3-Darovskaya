c736529246d1bdc2c445f9437866010cf52708e7
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 20:17:15 +0000

    Scripted auto-commit on change (2019-10-25 20:17:15) by gitwatch.sh

--- src/main/java/DelaysData.java
+++ src/main/java/DelaysData.java
@@ -10,7 +10,15 @@ public class DelaysData implements Serializable {
     }
 
     public DelaysData(String line) {
-        String[] 
+        Float max = Float.MIN_VALUE;
+        String[] delays = line.split(" ");
+        for (String delay : delays) {
+            if (!delay.equals(" ")) {
+                if (Float.parseFloat(delay) > max) {
+                    max = Float.parseFloat(delay);
+                }
+            }
+        }
     }
 
     public Float getMax() {
