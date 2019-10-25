541e3e61ba6b39ec09cfe5a752631b43a58cbaff
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 19:45:28 +0000

    Scripted auto-commit on change (2019-10-25 19:45:28) by gitwatch.sh

--- src/main/java/DelaysData.java
+++ src/main/java/DelaysData.java
@@ -4,10 +4,16 @@ public class DelaysData implements Serializable {
     private Float max;
     private Float percent;
 
-    public DelaysData(max, percent) {
+    public DelaysData() {
+        this.max = 0f;
+        this.percent = 0f;
+    }
+
+    public DelaysData(String linerx) {
         this.max = max;
         this.percent = percent;
     }
 
+    public Float
 
 }
