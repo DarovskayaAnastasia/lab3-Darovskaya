963368020745205293c90d2cddbff539049c267b
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 18:14:01 +0000

    Scripted auto-commit on change (2019-10-25 18:14:01) by gitwatch.sh

--- src/main/java/AirportsSparkMain.java
+++ src/main/java/AirportsSparkMain.java
@@ -1,3 +1,6 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class AirportsSparkMain {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
