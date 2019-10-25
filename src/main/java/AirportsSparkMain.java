93446f89b94e7edbba7a5d80fe34a281a6122cad
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 18:46:52 +0000

    Scripted auto-commit on change (2019-10-25 18:46:52) by gitwatch.sh

--- src/main/java/AirportsSparkMain.java
+++ src/main/java/AirportsSparkMain.java
@@ -19,14 +19,16 @@ public class AirportsSparkMain {
         JavaRDD<String> onTimeFile = sc.textFile("664600583_T_ONTIME_sample.csv");
 
         JavaPairRDD<String, Long> nameIDpair = airportsFile.mapToPair(s -> { String airportID = parseLine(s, 0);
-                                                                             Long airportName = (parseLine(s, 1));
-                                                                             return new Tuple2<>(airportID, airportName)});
+                                                                             Long airportName = Long.parseLong(parseLine(s, 1));
+                                                                             return new Tuple2<>(airportID, airportName);});
+
+        
 
     }
 
     private static String parseLine(String line, int numberOfRow) throws IOException {
 
-        String result = new String;
+        String result = "";
 
         CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
         for (CSVRecord record : parser) {
