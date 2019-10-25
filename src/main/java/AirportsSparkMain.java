f7457b4b598ec8013ab503125918cea858ce43df
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-25 18:20:43 +0000

    Scripted auto-commit on change (2019-10-25 18:20:43) by gitwatch.sh

--- src/main/java/AirportsSparkMain.java
+++ src/main/java/AirportsSparkMain.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportsSparkMain {
@@ -6,6 +7,8 @@ public class AirportsSparkMain {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        JavaRDD<String> airportsFile = sc.textFile("war-and-peace-1.txt");
+        JavaRDD<String> onTimeFile = sc.textFile("war-and-peace-1.txt");
         
     }
 }
